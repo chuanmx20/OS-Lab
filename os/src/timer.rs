70f651ff8d58ed742ac75b68c3a1444f8869691e
chuanmx <chuanmx20@mails.tsinghua.edu.cn>
2023-06-10 22:07:27 +0800

    rewrite get_time_us

--- os/src/timer.rs
+++ os/src/timer.rs
@@ -30,7 +30,7 @@ pub fn get_time_ms() -> usize {
 
 /// get current time in microseconds
 pub fn get_time_us() -> usize {
-    time::read() / (CLOCK_FREQ / MICRO_PER_SEC)
+    (time::read() * MICRO_PER_SEC) / CLOCK_FREQ 
 }
 
 /// Set the next timer interrupt
